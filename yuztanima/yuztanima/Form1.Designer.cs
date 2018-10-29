549d1d34a9242435c6e24353863f26e0cc2917f9
fatmaokcu <44584132+fatmaokcu@users.noreply.github.com>
2018-10-29 21:06:28 +0300

    Add files via upload

---
+++ yuztanima/yuztanima/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace yuztanima
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(410, 425);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(615, 198);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(154, 204);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(615, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(615, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "adet";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(615, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 63);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "resim kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ yuztanima/yuztanima/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+
+namespace yuztanima
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Capture capture = new Capture();
+            capture.Start();
+            capture.imagegrabbed+=(a,b)=>
+            {
+                var image = capture.RetrieveBgrFrame();
+                var image2 = image.Convert<Gray, byte>();
+                HaarCascade haaryuz = new HaarCascade("alt.xml");
+                MCvAvgComp[][] yuzler = image2.DetectHaarCascade(haaryuz, 1.2, 5, HAAR_DETECTİON_TYPE.DO_CANNY_PRUNING, new Size(15, 15));
+            }
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+
+                  {
+                      for (int i = 0; i < 10; i++)
+                      {
+                          if (!recognition.SaveTrainingData(pictureBox2.Image, textBox1.Text)) MessageBox.Show("hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                          Thread.Sleep(100);
+                          label1.Text = (i + 1) + "adet profil";
+                      }
+                      recognition = null;
+                      train = null;
+                      recognition = new BusinessRecognition("D:\\", "Faces", "yuz.xml");
+                      train = new Classifier_Train("D:\\", "Faces", "yuz.xml");
+
+                  });
+            }
+
+        BusinessRecognition recognition= new BusinessRecognition("D:\\", "Faces", "yuz.xml");
+        Classifier_Train train = new Classifier_Train("D:\\", "Faces", "yuz.xml");
+
+    }
+}
